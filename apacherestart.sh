0d9e293a998d97256af4ca8d28b351b028c69a41
JerronMcG <jerron.mcginnis@gmail.com>
2019-12-29 18:44:23 -0500

    Update apacherestart.sh

--- apacherestart.sh
+++ apacherestart.sh
@@ -7,3 +7,9 @@ if [ $? != 0 ]
 then
         /etc/init.d/apache2 start > /dev/null
 fi
+
+ps auxw | grep httpd | grep -v grep > /dev/null
+if [ $? != 0 ]
+then
+        /etc/init.d/httpd start > /dev/null
+fi
